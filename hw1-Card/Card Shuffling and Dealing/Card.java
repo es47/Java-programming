1d9d4b27fd6761ede8f3971cc4aa429b6e320f16
Lchs <kl8534ghj4633@gmail.com>
2018-08-24 17:38:59 +0800

    Create Card.java

---
+++ hw1-Card/Card Shuffling and Dealing/Card.java
@@ -0,0 +1,26 @@
+public class Card
+{
+	private final String face;
+	private final String suit;
+	
+	public Card (String cardFace, String cardSuit)
+	{
+		this.face = cardFace;
+		this.suit = cardSuit;
+	}
+	
+	public String toString()
+	{
+		return face + " of " + suit;
+	}
+	
+	public String getFace()
+	{
+		return face;
+	}
+	
+	public String getSuit()
+	{
+		return suit;
+	}
+}
